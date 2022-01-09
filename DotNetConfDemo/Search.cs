bde716b1d58f20a0e79c2e89808da0d6f06b97ec
Rishabh Verma <rishabhv@live.com>
2022-01-09 19:05:06 +0530

    .NET Conf 2022 code

---
+++ DotNetConfDemo/Search.cs
@@ -0,0 +1,113 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using Microsoft;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using Task = System.Threading.Tasks.Task;
+
+namespace DotNetConfDemo
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class Search
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("b344cc7d-0d60-4315-9d71-29ef03695fff");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Search"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private Search(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static Search Instance
+        {
+            get;
+            private set;
+        }
+        public static DTE2 dteInstance { get; private set; }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in Search's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+            dteInstance = await package.GetServiceAsync(typeof(DTE)) as DTE2;
+            Assumes.Present(dteInstance);
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new Search(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            var textSelection = dteInstance?.ActiveDocument?.Selection as TextSelection;
+            if(textSelection != null)
+            {
+                var text = textSelection.Text;
+                if(text != null)
+                {
+                    dteInstance.StatusBar.Text = $"searching for {text}";
+                    string urlEncodedText = System.Net.WebUtility.UrlEncode(text);
+                    string searchUrl = $"https://www.google.com/search?q={urlEncodedText}";
+                    System.Diagnostics.Process.Start(searchUrl);
+                    dteInstance.ItemOperations.Navigate(searchUrl);
+                }
+            }
+        }
+    }
+}
